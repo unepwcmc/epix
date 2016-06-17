06f9fe6c9919ff07f256856bfd4efa160bf687f1
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-06-17 11:11:16 +0100

    Create countries and adapters tables

--- db/migrate/20160616105528_create_countries.rb
+++ db/migrate/20160616105528_create_countries.rb
@@ -3,6 +3,8 @@ class CreateCountries < ActiveRecord::Migration
     create_table :countries do |t|
       t.string :name, null: false
       t.string :iso_code2, null: false
+
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20160616105832_create_adapters.rb
+++ db/migrate/20160616105832_create_adapters.rb
@@ -4,7 +4,21 @@ class CreateAdapters < ActiveRecord::Migration
       t.belongs_to :country, index: true
       t.string :name, null: false
       t.string :web_service_type
+      t.string :web_service_uri
+
+      ##Encrypted fields for authentication
+      t.string :encrypted_auth_token
+      t.string :encrypted_auth_token_iv
+      t.string :encrypted_auth_username
+      t.string :encrypted_auth_username_iv
+      t.string :encrypted_auth_password
+      t.string :encrypted_auth_password_iv
+
       t.boolean :is_available, default: false
+
+      t.timestamps null: false
     end
+
+    add_index :adapters, :name, unique: true
   end
 end
