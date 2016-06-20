0f26868a312fcc19bf10fcca8e35e13431357c21
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-06-20 10:55:23 +0100

    Rename timeout to time_out

--- lib/modules/adapters/simple_adapter.rb
+++ lib/modules/adapters/simple_adapter.rb
@@ -4,7 +4,7 @@ class Adapters::SimpleAdapter < Adapters::Base
     @params = {
       request_type: 'soap_request',
       wsdl: adapter.web_service_uri,
-      timeout: adapter.timeout,
+      timeout: adapter.time_out,
       operation: :get_non_final_cites_certificate,
       options: {},
       auth: {
