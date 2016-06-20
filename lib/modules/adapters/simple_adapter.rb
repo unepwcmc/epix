9d82edfc8aa685cf924603e25dcf8c1cf560a9c0
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-06-20 15:15:38 +0100

    Comment about how to invoke the adapter

--- lib/modules/adapters/simple_adapter.rb
+++ lib/modules/adapters/simple_adapter.rb
@@ -1,5 +1,14 @@
 class Adapters::SimpleAdapter < Adapters::Base
 
+  #Example on how to invoke the adapter
+  #
+  #  selected_country = 'Simple'
+  #  adapter = Country.find_by_name(selected_country).adapter
+  #  if adapter.is_available
+  #    @res = adapter.name.constantize.run(adapter)
+  #  else
+  #    #Return unavailable message
+  #  end
   def initialize(adapter)
     @params = {
       request_type: 'soap_request',
