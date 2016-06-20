a48db748dc2bc2b122a5b1599122e7953f593f40
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-06-20 13:55:46 +0100

    Added deploy

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'epix'
+set :repo_url, 'git@github.com:unepwcmc/epix.git'
+
+set :branch, 'master'
+
+set :deploy_user, 'wcmc'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.1'
+
+set :pty, true
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+set :linked_files, %w{config/database.yml .env}
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :keep_releases, 5
+
+set :passenger_restart_with_touch, false
