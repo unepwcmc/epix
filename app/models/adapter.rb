61b20984fff15a0c8634150936f8c5343294fc43
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-06-20 10:41:53 +0100

    Add attr_encrypted fields to adapter

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -1,2 +1,5 @@
 class Adapter < ActiveRecord::Base
+  attr_encrypted :auth_token, key: Rails.application.secrets.adapter["auth_token_key"]
+  attr_encrypted :auth_username, key: Rails.application.secrets.adapter["auth_username_key"]
+  attr_encrypted :auth_password, key: Rails.application.secrets.adapter["auth_password_key"]
 end
