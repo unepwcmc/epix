f91d24cc2223fa536a30582b000441ee0bb6e837
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-01 12:20:58 +0100

    added admin namespace and layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   resources :permits, only: [:index]
 
+  namespace :admin do
+  end
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
