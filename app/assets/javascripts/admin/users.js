e31fdd4800ad6a7d6e9b305dcf5ecdf86ae2073d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-03 14:03:07 +0200

    applied dataTables to users list

---
+++ app/assets/javascripts/admin/users.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#users_table").dataTable({
+    "bFilter": false,
+    "aoColumnDefs" : [{
+      "bSortable": false,
+      "aTargets": ["no-sort"]
+    }]
+  }).columnFilter({
+    sPlaceHolder: "head:before",
+    aoColumns: [
+      null,
+      {
+        type: "text",
+      },
+      {
+        type: "text",
+      },
+      {
+        type: "text",
+      },
+      {
+        type: "text",
+      },
+      null,
+      null,
+      null
+    ]
+  });
+});
