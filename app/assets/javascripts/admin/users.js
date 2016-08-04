9dc84d95beb516b21cd40e78855603eca429e390
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-04 13:38:12 +0200

    fix for DataTables warning: table id=users_table - Cannot reinitialise DataTable.

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,29 +1,34 @@
 $(document).on('turbolinks:load',function(){
-  $("#users_table").dataTable({
-    "bFilter": false,
-    "aoColumnDefs" : [{
-      "bSortable": false,
-      "aTargets": ["no-sort"]
-    }]
-  }).columnFilter({
-    sPlaceHolder: "head:before",
-    aoColumns: [
-      null,
-      {
-        type: "text"
-      },
-      {
-        type: "text"
-      },
-      {
-        type: "text"
-      },
-      {
-        type: "text"
-      },
-      null,
-      null,
-      null
-    ]
-  });
+  if ($.fn.dataTable.isDataTable('#users_table')) {
+    table = $('#users_table').dataTable();
+  }
+  else {
+    $("#users_table").dataTable({
+      "bFilter": false,
+      "aoColumnDefs" : [{
+        "bSortable": false,
+        "aTargets": ["no-sort"]
+      }]
+    }).columnFilter({
+      sPlaceHolder: "head:before",
+      aoColumns: [
+        null,
+        {
+          type: "text"
+        },
+        {
+          type: "text"
+        },
+        {
+          type: "text"
+        },
+        {
+          type: "text"
+        },
+        null,
+        null,
+        null
+      ]
+    });
+  }
 });
