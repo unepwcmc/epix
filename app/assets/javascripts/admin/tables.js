637027d0261fe52a1640e6552412f8bbaa2e679b
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-24 17:59:44 +0200

    same data tables initialisation code for users and organisations

--- app/assets/javascripts/admin/tables.js
+++ app/assets/javascripts/admin/tables.js
@@ -1,14 +1,26 @@
 $(document).on('turbolinks:load',function(){
-  var tables = [
-    $('#users_table').DataTable(),
-    $('#organisations_table').DataTable()
-  ];
+  var tableElementIds = ['#users_table', '#organisations_table'];
+  for (var i = 0; i < tableElementIds.length; i++) {
+    var tableElementId = tableElementIds[i];
+    if ($.isEmptyObject($.find(tableElementId))){
+      continue;
+    }
+    var table;
+    if ($(tableElementId).attr('data-table-initialised')) {
+      table = $(tableElementId).DataTable();
+    } else {
+      table = $(tableElementId).DataTable({
+        "aoColumnDefs" : [{
+          "bSortable": false,
+          "aTargets": ["no-sort"]
+        }]
+      });
+      $(tableElementId).attr('data-table-initialised', 1);
+    }
 
-  for(var i = 0; i < tables.length; i++) {
-    if(tables[i].data().length > 0) {
-      tables[i].columns('.searchable').every(function() {
+    if (table.data().length > 0) {
+      table.columns('.searchable').every(function() {
         var column = this;
-
         $('input', this.footer()).on('keyup change', function () {
           if (column.search() !== this.value) {
             column.search( this.value ).draw();
