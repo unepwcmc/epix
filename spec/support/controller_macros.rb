e38d152f8f617ed1e31da5305b6f17e771b9c57f
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-24 14:03:43 +0200

    Login admin and create organisations macros

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,9 +1,32 @@
 module ControllerMacros
   def login_user
+    create_organisations
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
+      user = FactoryGirl.create(:user, organisation_id: @cites_ma.id)
       sign_in user
+    end
+  end
+
+  def login_admin
+    create_organisations
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user,
+        is_admin: true,
+        organisation_id: @cites_ma.id
+      )
+      sign_in user
+    end
+  end
+
+  def create_organisations
+    before(:each) do
+      Organisation::VALID_ROLES.each do |role|
+        role_name = role.downcase.tr(" ", "_")
+        organisation = FactoryGirl.create(:organisation, role: role)
+        instance_variable_set("@#{role_name}", organisation)
+      end
     end
   end
 end
