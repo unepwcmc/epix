26041176ca9a51f4fac981a7649e0f7ac9222eed
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-24 11:38:31 +0200

    Add set of abilities for user

---
+++ app/models/ability.rb
@@ -0,0 +1,20 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.is_admin
+      if user.is_system_managers?
+        can :manage, :all
+      elsif user.is_cites_ma?
+        can :update, Organisation, id: user.organisation.id
+        can :update, Adapter, id: user.organisation.adapter.id
+        can :manage, User, organisation_id: user.organisation_id
+      elsif user.is_customs_ea?
+        can :update, Organisation, id: user.organisation.id
+        can :manage, User, organisation_id: user.organisation_id
+      end
+    else
+      can :update, User, id: user.id
+    end
+  end
+end
