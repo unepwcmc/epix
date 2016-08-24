4acae765270c7f75ac452dd4e40f14a626ff8249
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-24 11:19:19 +0200

    added specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has an invalid email" do
+    user = FactoryGirl.build(:user, email: 'joe.doe.email')
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without first name" do
+    expect(FactoryGirl.build(:user, first_name: nil)).to be_invalid
+  end
+
+  it "is invalid without last name" do
+    expect(FactoryGirl.build(:user, last_name: nil)).to be_invalid
+  end
+
+  it "is invalid without email" do
+    expect(FactoryGirl.build(:user, email: nil)).to be_invalid
+  end
+
   subject { FactoryGirl.create(:user) }
 
   it 'sends an email' do
