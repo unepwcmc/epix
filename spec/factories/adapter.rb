e32b0bf1f69cd0194b17fdc6eee47de89badd1b3
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-24 15:49:55 +0200

    Refactor adapter factory to avoid existing names

--- spec/factories/adapter.rb
+++ spec/factories/adapter.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :adapter do
-    name { Faker::Superhero.name }
+    sequence(:name) { |n| "#{n}#{Faker::Superhero.name}" }
     web_service_type 'SOAP'
     web_service_uri { Faker::Internet.url }
     is_available true
