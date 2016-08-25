6910065a287b91e967462a00fc12c226d7cd09bd
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-25 10:10:03 +0200

    Fix organisation for users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,12 +5,12 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     is_admin false
-    organisation FactoryGirl.create(:cites_ma)
+    organisation { FactoryGirl.create(:cites_ma) }
 
     Organisation::VALID_ROLES.each do |role|
       role_name = role.downcase.tr(" ", "_")
       factory :"#{role_name}_user" do
-        organisation FactoryGirl.create(role_name.to_sym)
+        organisation { FactoryGirl.create(role_name.to_sym) }
         is_admin true
       end
     end
