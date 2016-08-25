4c217889a86712291d10d3c2990330b1f532e12e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-25 11:57:04 +0200

    Rescue from CanCan::AccessDenied and redirect with flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, notice: exception.message
+  end
+
   private
 
   def set_locale
