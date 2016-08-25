51ed22bec08140fd9d002e77be0849bab394a1f2
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-08-25 10:10:55 +0200

    Amend ability when looking for adapter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
         can :manage, :all
       elsif user.is_cites_ma?
         can :update, Organisation, id: user.organisation.id
-        can :update, Adapter, id: user.organisation.adapter.id
+        can :update, Adapter, id: user.organisation.try(:adapter).try(:id)
         can :manage, User, organisation_id: user.organisation_id
       elsif user.is_customs_ea?
         can :update, Organisation, id: user.organisation.id
