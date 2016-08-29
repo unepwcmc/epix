e2c222201919e1fa5ad563b2ee8b07a0d9736a13
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-08-29 17:58:06 +0200

    select from list of countries with available adapters

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -1,6 +1,9 @@
 class PermitsController < ApplicationController
   def index
-    @countries = Country.select(:id, :name)
+    @countries = Organisation.cites_mas.with_available_adapters.
+      joins(:country).
+      select('countries.iso_code2, countries.name').
+      group('countries.iso_code2, countries.name')
   end
 
   def show
