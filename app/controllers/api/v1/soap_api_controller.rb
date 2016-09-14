6732486a8e5a1922b6d27dbd0987d4d342b8520e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-09-14 10:30:40 +0100

    load adapter except when action is wsdl

--- app/controllers/api/v1/soap_api_controller.rb
+++ app/controllers/api/v1/soap_api_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::SoapApiController < Api::V1::BaseController
     return user.valid_password?(password)
   }
 
-  before_action :load_adapter
+  before_action do
+    load_adapter if request.original_url.split('/').last != 'wsdl'
+  end
 
   soap_action :get_final_cites_certificate,
               args: {
