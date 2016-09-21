fd27f19c2d462430c18d551b5eb6fb07c4d1d859
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-09-21 10:15:06 +0100

    Remove trailing comma

--- app/assets/javascripts/admin/organisations.js
+++ app/assets/javascripts/admin/organisations.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load',function(){
   var $countries_input = $('#countries_with_access')
   $countries_input.select2({
     placeholder: "Select tag",
-    minimumInputLength: 1,
+    minimumInputLength: 1
   });
   $($countries_input.data('tags')).each(function (index, country) {
     var option = new Option(country.text, country.id, true, true);
