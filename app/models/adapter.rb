a7f18ba3ac3894c5ca2e7547e85ce2c4da4c00e5
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-09-21 10:03:49 +0100

    Remove unused association

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -4,5 +4,4 @@ class Adapter < ApplicationRecord
   attr_encrypted :auth_password, key: Rails.application.secrets.adapter["auth_password_key"]
 
   belongs_to :organisation
-  has_many :countrues, foreign_key: 'countries_with_access_ids'
 end
