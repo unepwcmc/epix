2f9ea99d916eb1eef8d1366ceac44390e6d08a0e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-02 16:15:47 +0000

    Show value and checkbox for new field

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -8,4 +8,11 @@ module OrganisationsHelper
     return no unless @adapter
     @adapter.has_country?(country.id) ? yes : no
   end
+
+  def trade_reporting_permitted?
+    yes = content_tag(:span, '', class: 'fa fa-check')
+    no = content_tag(:span, '', class: 'fa fa-times')
+    return no unless @adapter
+    @organisation.trade_reporting_unpermitted ? no : yes
+  end
 end
