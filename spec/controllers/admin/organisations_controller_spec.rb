a50193976b6f248adc081815f6ca3b064cf3e626
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-12-06 14:09:53 +0000

    fixed deprecation warnings

--- spec/controllers/admin/organisations_controller_spec.rb
+++ spec/controllers/admin/organisations_controller_spec.rb
@@ -127,13 +127,15 @@ RSpec.describe Admin::OrganisationsController, type: :controller do
     describe "PATCH update" do
       it "can update access list of countries" do
         organisation = subject.current_user.organisation
-        patch :update, id: organisation.id,
+        patch :update, params: {
+          id: organisation.id,
           organisation: {
             adapter_attributes: {
               id: organisation.adapter.id,
               countries_with_access_ids: [1, 2]
             }
           }
+        }
         organisation.reload
         expect(organisation.adapter.countries_with_access_ids).to eq([1, 2])
       end
@@ -146,13 +148,15 @@ RSpec.describe Admin::OrganisationsController, type: :controller do
     describe "PATCH update" do
       it "cannot update access list of countries" do
         organisation = subject.current_user.organisation
-        patch :update, id: organisation.id,
+        patch :update, params: {
+          id: organisation.id,
           organisation: {
             adapter_attributes: {
               id: organisation.adapter.id,
               countries_with_access_ids: [1, 2]
             }
           }
+        }
         organisation.reload
         expect(organisation.adapter.countries_with_access_ids).to eq([])
       end
