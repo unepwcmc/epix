41a374cd81d570f2c41384adeb08ca7789f4362e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-14 09:48:27 +0000

    Token info in translation file

--- app/helpers/permits_helper.rb
+++ app/helpers/permits_helper.rb
@@ -1,10 +1,8 @@
 module PermitsHelper
-  TOKEN_INFO =
-    'Please fill in for permit numbers protected by an additional security code.'
   def security_token_label
     label_tag(:security_token, class: 'security-token-label') do
       content_tag(:span, t('enter_security_token')) +
-      content_tag(:i, ' ', class: 'fa fa-info-circle blue', title: TOKEN_INFO)
+      content_tag(:i, ' ', class: 'fa fa-info-circle blue', title: t('token_info'))
     end
   end
 end
